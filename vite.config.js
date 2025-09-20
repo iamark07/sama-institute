ce803c8913c33fc5c0684ba52fb6e23766772be3
Arbaz Karimi <arbazkarimi@gmail.com>
2025-09-20 16:42:08 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/sama-institute/",
+  base: process.env.NODE_ENV === "production" ? "/sama-institute/" : "/",
   plugins: [
     react(),
     tailwindcss(),
