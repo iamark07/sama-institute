4bb389c4a4a98080ac2a1944301a297aea6eccb4
Arbaz Karimi <arbazkarimi@gmail.com>
2025-10-23 18:09:21 +0530

    latest code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
       <div className="p-4">
         Home Page Content
         <div className="mt-4">
-          <Link to="/organization-registration" className="text-blue-600 hover:underline">Go to Organization Registration Page</Link>
+          <Link to="/" className="text-blue-600 hover:underline">Go to Login Page</Link>
         </div>
       </div>
     </>
